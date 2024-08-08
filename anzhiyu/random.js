383f3cc86db83730f2a4a45003ebf5277e66e9fe
韩明(侯马厂) <00075740@HMCNB00000013.local>
2024-08-08 10:52:51 +0800

    Site updated: 2024-08-08 10:52:51

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/07/18/hello-world/"];function toRandomPost(){
+var posts=["undefined/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
